4d288b04699ffa7074dc76d723412bbf2da7c266
pablo <pablo@DESKTOP-3HQUQ7H.mshome.net>
2021-03-20 09:40:05 -0600

    Guia 4

---
+++ src/main/java/Guia4/Operaciones.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Guia4;
+
+/**
+ *
+ * @author pablo
+ */
+public abstract class Operaciones {
+    public abstract int suma(int a, int b);
+    public abstract int resta(int a, int b);
+    public abstract int division(int a, int b);
+    public abstract int multiplicacion(int a, int b);
+}
